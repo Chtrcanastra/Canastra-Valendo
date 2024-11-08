dda6da8f3da5684a737980310c9114bccf64e2f0
Chtrcanastra <canastravalendo@gmail.com>
2024-11-08 03:46:16 -0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+@file:Suppress("UNUSED_EXPRESSION", "UNUSED_EXPRESSION")
+
+import kotlin.Any as KotlinAny
+
+fun PluginContainer.get() = Unit
+
+fun append() = Unit
+
+fun <T> Comparable<T>.getBuildString(): KotlinAny = Unit
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+val id: String
+    get() = ""
+
